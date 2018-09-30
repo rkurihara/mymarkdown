7ee4f48865556af8b10e6da9e2c97d9df4387803
rkurihara <lalalalyla.rk@gmail.com>
2018-09-30 14:39:24 +0900

    add markdwon editor

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -3,24 +3,46 @@
     <h1>エディター画面</h1>
     <span>{{user.displayName}}</span>
     <button @click="logout">ログアウト</button>
+    <div class="editorWrapper">
+      <textarea class="markdown" v-model="markdown"></textarea>
+      <div class="preview" v-html="preview()"></div>
+    </div>
   </div>
 </template>
 
 <script>
+import marked from 'marked';
+
 export default {
   name: "editor",
   props: ["user"],
   data() {
-    return {}
+    return {
+      markdown: '',
+    }
   },
   methods: {
     logout: function() {
       firebase.auth().signOut();
-    }
+    },
+    preview: function() {
+      return marked(this.markdown);
+    },
   }
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
+.editorWrapper{
+  display: flex;
+}
+.markdown {
+  width: 50%;
+  height: 500px;
+}
+.preview {
+  widows: 50%;
+  text-align: left;
+}
 </style>
 
