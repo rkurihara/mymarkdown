01e9988b474a97c5c5add61971e0a557a2bc6fb1
rkurihara <lalalalyla.rk@gmail.com>
2018-09-30 16:46:44 +0900

    save memo

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -9,6 +9,7 @@
       </div>
       <button class="addMemoBtn" @click="addMemo"> メモの追加 </button>
       <button class="deleteMemoBtn" v-if="memos.length > 1" @click="deleteMemo"> 選択中のメモの削除 </button>
+      <button class="saveMemosBtn" @click="saveMemos"> メモの保存 </button>
     </div>
     <textarea class="markdown" v-model="memos[selectedIndex].markdown"></textarea>
     <div class="preview" v-html="preview()"></div>
@@ -28,6 +29,17 @@ export default {
       }],
       selectedIndex: 0
     }
+  },
+  created: function() {
+    firebase
+      .database()
+      .ref('memos/' + this.user.uid)
+      .once('value')
+      .then(result => {
+        if(result.val()){
+          this.memos = result.val();
+        }
+      })
   },
   methods: {
     logout: function() {
@@ -52,6 +64,12 @@ export default {
       if(this.selectedIndex > 0) {
         this.selectedIndex--;
       }
+    },
+    saveMemos: function() {
+      firebase
+        .database()
+        .ref('memos/' + this.user.uid)
+        .set(this.memos);
     },
   }
 }
@@ -83,6 +101,9 @@ export default {
 }
 .addMemoBtn {
   margin-top: 20px;
+}
+.deleteMemoBtn {
+  margin: 10px;
 }
 .markdown {
   float: left;
